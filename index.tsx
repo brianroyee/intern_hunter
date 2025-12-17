4f8965cd31d2800a2d6f1405ac6cbd683d3dc574
brianroyee <briancodee@gmail.com>
2025-12-17 15:28:29 +0530

    feat: Implement blog post viewing with dedicated pages and new API for content management.

--- index.tsx
+++ index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Admin from "./Admin";
-import Blogs from "./BlogsPage";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -14,11 +12,7 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/blogs" element={<Blogs />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
